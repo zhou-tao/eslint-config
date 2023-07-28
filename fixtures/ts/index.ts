e28973c13504452c04d110bf33ab0da99b797ed6
zhou-tao <zhoutao_233@163.com>
2023-07-28 18:07:19 +0800

    feat: add react config

--- fixtures/ts/index.ts
+++
@@ -1,3 +0,0 @@
-export function install(options: any) {
-  console.log(options)
-}
--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,5 +1,14 @@
 module.exports = {
   extends: [
-    '@toryz/eslint-config-basic'
-  ]
+    '@toryz/eslint-config-ts',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ]
+  }
 }
